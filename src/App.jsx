79a183513e58eacec9bc14d535bfe0b34efc8c8c
PGSHarshaVardhan <2300031220@kluniversity.in>
2025-09-13 08:48:52 +0530

    jenkinsapp demo

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1> CICD JENKINS DEMO</h1>
+        <p> jenkins source code  uses git repo  and set branch /main   poll scm triggers option  * * * * * buid step add buid step <b> Windows batch command </b> <u>call npm install </u>  <u> call npm run dev</u> rmdir "C:\......tomcat9\webapps\jenkinsfrontdemo
+  mkdir "C:\......tomcat9\webapps\jenkinsfrontdemo
+  xcopy path </p>
+  
+  </div>
+  <div>
+  <p> buid step add buid step <b> Windows batch command</b>
+  stop tomcat 9
+  start tomcat 9
+  </p>
+      </div>
+      
+    </>
+  );
+}
+
+export default App
